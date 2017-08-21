1bf1baca86a892ad42e7a38e7cfa387617dca388
Froylan  Vargas Gomez <froy@MacBook-Pro-de-Froylan.local>
2017-08-21 15:21:46 -0500

    Mail, lost image, last styles

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,13 @@ $(function () {
         $(".navbar-toggle").click();
     });
 });
+
+function sendMail() {
+    var mensaje = $("#mensaje").val();
+
+    $(location).attr('href', 'mailto:example@example.com?subject='
+    + encodeURIComponent("Alkimia Contacto")
+    + "&body=" 
+    + encodeURIComponent(mensaje)
+    );
+}
